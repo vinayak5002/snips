9210d555e5eff3e10ad1a831e303aaf12367f116
Vinayak <mailvizzard@gmail.com>
2024-10-25 12:24:20 +0530

    fix: fix issue #5

---
+++ client/src/utils/utils.ts
@@ -0,0 +1,36 @@
+
+export function timeAgo(dateString: string): string {
+  
+  if(dateString == null) return "Never";
+
+  const now = new Date();
+  const pastDate = new Date(dateString);
+  const seconds = Math.floor((now.getTime() - pastDate.getTime()) / 1000);
+
+  console.log("Last indexed x seconds ago: ", seconds);
+
+  let interval: number;
+
+  interval = Math.floor(seconds / 31536000); // years
+  if (interval > 1) return `${interval} years ago`;
+  if (interval === 1) return `1 year ago`;
+
+  interval = Math.floor(seconds / 2592000); // months
+  if (interval > 1) return `${interval} months ago`;
+  if (interval === 1) return `1 month ago`;
+
+  interval = Math.floor(seconds / 86400); // days
+  if (interval > 1) return `${interval} days ago`;
+  if (interval === 1) return `1 day ago`;
+
+  interval = Math.floor(seconds / 3600); // hours
+  if (interval > 1) return `${interval} hours ago`;
+  if (interval === 1) return `1 hour ago`;
+
+  interval = Math.floor(seconds / 60); // minutes
+  if (interval > 1) return `${interval} mins ago`;
+  if (interval === 1) return `1 min ago`;
+
+  return seconds < 5 ? "few seconds ago" : `${seconds} seconds ago`;
+}
+
